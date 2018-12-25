900d7eeedba2b91056bf4eb91252415af56fc161
xiebiqi <xiebiqi@51yryc.com>
2018-12-25 16:51:38 +0800

    初始化

---
+++ delete/src/main/java/yryc/logic/delete/DeleteApplication.java
@@ -0,0 +1,14 @@
+package yryc.logic.delete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteApplication.class, args);
+	}
+
+}
+
